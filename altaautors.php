1fabf0ca79cd34a46a15df0f84c22e5f4353fc8f
SSJordiSS <jorgesimo@iesmontsia.org>
2017-11-30 15:24:17 +0100

    Merge branch 'master' of https://github.com/rogerforner/M07_UF3_ABP1_biblioteca

--- altaautors.php
+++ altaautors.php
@@ -3,4 +3,5 @@ include_once('classes/autors.php');
 $autors = new autors($_POST["nom"],$_POST["cognom"],$_POST["nacionalitat"]);
 
 $autors-> introduirdades();
+header("Location:Formularis_html/Crear_autors.php")
 ?>

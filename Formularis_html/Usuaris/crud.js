0008951cf6723edd8b81ec45c8fbde5dcc5abe26
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-12-03 11:29:35 +0100

    usuaris AJAX Afegir Ok

--- Formularis_html/Usuaris/crud.js
+++ Formularis_html/Usuaris/crud.js
@@ -9,21 +9,35 @@
 # Afegir
 ------------------------------------------------------------------------------*/
 function afegirUsuari() {
+	formData = $("form#afegirUsuari").serialize();
   
-}
+  $.ajax({
+  	type: "POST",
+  	url: "Usuaris/altausuaris.php",
+  	data: formData,
+  })
+  .done(function(data, textStatus, jqXHR) {
+  	if (console && console.log) {
+  		console.log("La sol·licitud s'ha completat correctament.");
+  	}
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+  	if (console && console.log) {
+  		console.log("La sol·licitud ha fallat: " + textStatus);
+  	}
+  });
+} //fi_function
 
 
 /*
 # Eliminar
 ------------------------------------------------------------------------------*/
 function eliminarUsuari() {
-  
-}
+} //fi_function
 
 
 /*
 # Editar
 ------------------------------------------------------------------------------*/
 function editarUsuari() {
-  
-}
+} //fi_function
